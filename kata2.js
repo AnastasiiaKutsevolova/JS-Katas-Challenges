fb20aa137477f9b0d496ab419d424b5e727729cc
Anastasia Kutsevolova <anastasiasemenok@gmail.com>
2022-06-07 21:13:47 -0600

    challenge 8

--- kata2.js
+++ kata2.js
@@ -87,3 +87,22 @@ const intersection = (...arrays) => {
 console.log(
   intersection([5, 10, 15, 20], [15, 88, 1, 5, 7], [1, 10, 15, 5, 20])
 );
+
+// Challenge 8
+const conditionalSum = function (arr, condition) {
+  const add = (num1, num2) => {
+    if (condition === "even" && num2 % 2 === 0) {
+      return num1 + num2;
+    }
+    if (condition === "odd" && num2 % 2 === 1) {
+      return num1 + num2;
+    }
+    return num1;
+  };
+  return arr.reduce((acc, val) => add(acc, val), 0);
+};
+
+console.log(conditionalSum([1, 2, 3, 4, 5], "even"));
+console.log(conditionalSum([1, 2, 3, 4, 5], "odd"));
+console.log(conditionalSum([13, 88, 12, 44, 99], "even"));
+console.log(conditionalSum([], "odd"));
